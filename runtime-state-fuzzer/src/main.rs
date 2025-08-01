a4715d31eaa58a9c87e22442a30e96b4bf17bd5f
vgantchev <v@lery.dev>
2025-08-01 16:46:36 +0200

    put pid snapshots in output

--- runtime-state-fuzzer/src/main.rs
+++ runtime-state-fuzzer/src/main.rs
@@ -33,7 +33,7 @@ use std::cell::RefCell;
 use std::process;
 
 fn snapshot_path_for_pid() -> String {
-    format!("data/snapshot-{}.bin", process::id())
+    format!("output/pid_snapshots/snapshot-{}.bin", process::id())
 }
 
 thread_local! {
